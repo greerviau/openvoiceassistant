40de1bff7ab33171d166309a0d2217fdb1e94eed
Greer <gviau2@gmail.com>
2021-12-28 01:06:33 -0500

    Update volume_control.py

--- skills/volume_control.py
+++ skills/volume_control.py
@@ -3,7 +3,7 @@ from subprocess import call
 def set_volume(volume, device):
     scale = int(255.0 * (volume/100.0))
     print(scale)
-    call(["amixer", '-D', f'hw:{device}', 'sset', 'Playback', f'{scale}%'])
+    call(["amixer", '-D', f'hw:{device}', 'sset', 'Playback', f'{scale}'])
 
 def scale_volume(scale):
     m = alsaaudio.Mixer('Headphone')
