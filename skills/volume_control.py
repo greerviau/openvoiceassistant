322a3985127410130a6e0ea44b89e78296b5c8b2
Greer <gviau2@gmail.com>
2021-12-28 01:08:13 -0500

    Update volume_control.py

--- skills/volume_control.py
+++ skills/volume_control.py
@@ -1,7 +1,7 @@
 from subprocess import call
 
 def set_volume(volume, device):
-    scale = int(255.0 * (volume/100.0))
+    scale = 200 + int(55.0 * (volume/100.0))
     print(scale)
     call(["amixer", '-D', f'hw:{device}', 'sset', 'Playback', f'{scale}'])
 
