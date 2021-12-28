06377dd5b7a19ac885c1cbdd95432a6be5979228
Greer <gviau2@gmail.com>
2021-12-27 22:44:51 -0500

    Update volume_control.py

--- skills/volume_control.py
+++ skills/volume_control.py
@@ -1,11 +1,11 @@
 def set_volume(volume):
     import alsaaudio
-    m = alsaaudio.Mixer('PCM')
+    m = alsaaudio.Mixer('Headphone')
     m.setvolume(volume)
 
 def scale_volume(scale):
     import alsaaudio
-    m = alsaaudio.Mixer('PCM')
+    m = alsaaudio.Mixer('Headphone')
     volume = m.getvolume()
     volume = volume * scale
     m.setvolume(volume)
