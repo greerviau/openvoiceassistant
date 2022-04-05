4f9d42c2b4131ec54eb5b84eae709256168df34c
Greer <gviau2@gmail.com>
2022-04-05 01:52:08 -0400

    move net_scan

--- utils.py
+++ utils.py
@@ -32,3 +32,18 @@ def pad_sequence(encoded, seq_length):
     else:
         padding[:len(encoded)] = encoded
     return padding
+
+def net_scan(ip):
+    arp_req_frame = scapy.ARP(pdst = ip)
+
+    broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    
+    broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
+
+    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
+    result = []
+    for i in range(0,len(answered_list)):
+        client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc}
+        result.append(client_dict)
+
+    return result
--- virtual_assistant_client.py
+++ virtual_assistant_client.py
@@ -11,7 +11,7 @@ import os
 import json
 import queue
 import requests
-from utils import clean_text
+from utils import clean_text, net_scan
 import scapy.all as scapy
 import wave
 import base64
@@ -93,22 +93,6 @@ class VirtualAssistantClient(threading.Thread):
         self.skills = {
             'set_volume':volume_control.set_volume
         }
-        
-    @staticmethod
-    def net_scan(ip):
-        arp_req_frame = scapy.ARP(pdst = ip)
-
-        broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-        
-        broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
-
-        answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
-        result = []
-        for i in range(0,len(answered_list)):
-            client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc}
-            result.append(client_dict)
-
-        return result
 
     def scan_for_hub(self, port):
         devices = net_scan('10.0.0.1/24')
