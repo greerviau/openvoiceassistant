032d65260f41df26a39f05b6b539f19b1e2f504c
Greer <gviau2@gmail.com>
2022-04-05 02:00:25 -0400

    import pymqtt

--- virtual_assistant_client.py
+++ virtual_assistant_client.py
@@ -14,6 +14,7 @@ import requests
 from utils import clean_text, net_scan
 import wave
 import base64
+import paho.mqtt.client as mqtt
 from skills import volume_control
 
 class VirtualAssistantClient(threading.Thread):
