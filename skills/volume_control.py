ca2f226eca08e8b52c4d8505460b37d9a45109ac
Greer <gviau2@gmail.com>
2022-09-02 13:23:14 -0400

    clean out legacy stuff

--- skills/volume_control.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-
-def set_volume(volume, device):
-    '''
-    scale = 200 + int(55.0 * (volume/100.0))
-    if volume == 0:
-        scale = 0
-    '''
-    call(['sudo', 'amixer', 'cset', 'numid=1', f'{volume}%'])
--- test_scripts/test_nlp.py
+++
@@ -1,4 +0,0 @@
-from subject_verb_object_extract import findSVOs, nlp
-tokens = nlp("Remind me to call Bank Gloucester at 3pm tomorrow")
-svos = findSVOs(tokens)
-print(svos)
